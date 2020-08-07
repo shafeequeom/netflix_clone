065b7a7008fa878db440139b37880d70aa9690ed
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2020-08-08 02:26:52 +0400

    Navbar Changes

---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
+        alt="Netflix"
+        className="nav__logo"
+      />
+      <img
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt=""
+        className="nav__avatar"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
