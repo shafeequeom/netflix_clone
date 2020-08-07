d70a85260cb26049d7999861b032a6314a1323df
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2020-08-08 02:02:15 +0400

    Row and Banner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import Row from "./Row.js";
+import Banner from "./Banner.js";
+import Navbar from "./Navbar.js";
+import requests from "./request";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRaterd} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMvoies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default App;
