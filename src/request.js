a02dae45ec4cc2c1556beb5124e944f39061a90e
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2020-08-07 23:38:33 +0400

    Axios and API Endpoints Setup

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "71f56373150f5f9ab31f5fa0e4442e79";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRaterd: `/movie/top-rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMvoies: `/discover/movie/week?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie/week?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie/week?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie/week?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie/week?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
